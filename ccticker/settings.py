aa11196302de3f49e1459cf8d22119451d283f1e
madhur73 <madhuratnyc@gmail.com>
2020-08-07 10:51:27 -0400

    github link attached

--- ccticker/settings.py
+++ ccticker/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import environ
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -20,7 +21,6 @@ environ.Env.read_env()
 DEBUG = env('DEBUG')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -31,8 +31,7 @@ SECRET_KEY = '*#ot6ux2#v%zm$kc!z9nwxb&&hj0oxgxystd5+z#z1!*+$$b7-'
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['intense-shelf-05236.herokuapp.com','127.0.0.1']
-
+ALLOWED_HOSTS = ['intense-shelf-05236.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -47,7 +46,6 @@ INSTALLED_APPS = [
     'ccapp.apps.CcappConfig',
     'django_celery_beat',
     'celery.contrib.testing',
-    
 
 ]
 ASGI_APPLICATION = "ccticker.routing.application"
@@ -82,7 +80,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ccticker.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
@@ -92,7 +89,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -112,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -125,7 +120,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
@@ -145,7 +139,6 @@ CACHES = {
 
 CELERY_BROKER_URL = env('REDIS_URL')
 CELERY_RESULT_BACKEND = env('REDIS_URL')
-
 
 CHANNEL_LAYERS = {
     "default": {
